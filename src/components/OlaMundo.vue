9f32ff3254517661d88b55bd49f2efa9d1be7bf2
GUSTAVO DE ARAUJO SILVA <gustavodzn@Live.com>
2022-10-28 21:01:32 -0300

    ALteracao 1

---
+++ src/components/OlaMundo.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Ola mundo</h1>
+    <h2>Primeiro projeto em vue3</h2>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from "vue"
+
+export default defineComponent({
+    name: "olaMundo"
+})
+
+</script>
+
+<style scope>
+h1 {
+    color: blue;
+}
+
+</style>
